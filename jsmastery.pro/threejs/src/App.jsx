0e4d077770dc45547c61c3f3557a2c49e348077c
bebopkenny <kennygarcia15@csu.fullerton.edu>
2024-12-11 05:09:29 -0800

    3D

--- jsmastery.pro/threejs/src/App.jsx
+++ jsmastery.pro/threejs/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Navbar from "./sections/Navbar.jsx";
 import Hero from "./sections/Hero.jsx"; // Import Hero component
+import About from "./sections/About.jsx"; // Import About component
+import Projects from './sections/Projects.jsx'; // Import Projects component
 
 const App = () => {
   return (
     <main className="max-w-7xl mx-auto">
       <Navbar />
       <Hero />
+      <About /> 
+      <Projects />  
     </main>
   );
 };
--- jsmastery.pro/threejs/src/constants/index.js
+++ jsmastery.pro/threejs/src/constants/index.js
@@ -58,11 +58,11 @@ export const clientReviews = [
 
 export const myProjects = [
   {
-    title: 'Podcastr - AI Podcast Platform',
-    desc: 'Podcastr is a revolutionary Software-as-a-Service platform that transforms the way podcasts are created. With advanced AI-powered features like text-to-multiple-voices functionality, it allows creators to generate diverse voiceovers from a single text input.',
+    title: 'SympToDialog - AI chatbot for healthcare',
+    desc: 'Developed an AI-powered chatbot for healthcare provider training, enhancing diagnostic skills in sensitive STI conversations, using JavaScript, Node.js, and OpenAI API for realistic dialogue generation.',
     subdesc:
-      'Built as a unique Software-as-a-Service app with Next.js 14, Tailwind CSS, TypeScript, Framer Motion and Convex, Podcastr is designed for optimal performance and scalability.',
-    href: 'https://www.youtube.com/watch?v=zfAb95tJvZQ',
+      'Built a scalable application architecture supporting real-time interaction and secure data management, leveraging Next.js and Firebase for seamless performance and user authentication.',
+    href: 'https://github.com/anonymousegoose/symptodialogue',
     texture: '/textures/project/project1.mp4',
     logo: '/assets/project-logo1.png',
     logoStyle: {
